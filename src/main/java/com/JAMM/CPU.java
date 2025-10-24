937eed517b9c652127b8516bf61b9eb94b87bd17
Mathieugril <mathieugril31@gmail.com>
2025-10-24 12:20:45 +0100

    cpu test 2

--- src/main/java/com/JAMM/CPU.java
+++ src/main/java/com/JAMM/CPU.java
@@ -11,6 +11,7 @@ import oshi.hardware.HardwareAbstractionLayer;
 public class CPU {
 
     public static CentralProcessor getCpuCores() {
+           Scanner ise = new Scanner(System.in);
        
         // creates a syateminfo object and hardwareabstract object
         SystemInfo si = new SystemInfo();
@@ -28,25 +29,85 @@ public class CPU {
        Scanner ise = new Scanner(System.in);
         
         System.out.print("What would you like to know about the CPU? ");
-            String input = ise.nextLine().toLowerCase();
+            String input1 = ise.nextLine().toLowerCase();
 
-        if ((input.contains("cores"))) {
+        if ((input1.contains("cores"))) {
          //  System.out.println("Processor: " + processor.getProcessorIdentifier().getName());
-            System.out.println("Physical Cores: " + processor.getPhysicalProcessorCount());
+           
+         System.out.println("Physical Cores: " + processor.getPhysicalProcessorCount());
              System.out.println("Logical Cores: " + processor.getLogicalProcessorCount());
         } 
-        else if ((input.contains("frequency")) || (input.contains("freq"))) {
+        
+        else if ((input1.contains("frequency")) || (input1.contains("freq"))) {
+           
             System.out.println("Max Frequency: " + processor.getMaxFreq() + " GHz");
         } 
-        else if ((input.contains("cache")) || (input.contains("caches"))) {
-            List<ProcessorCache> caches = processor.getProcessorCaches();
-            for (ProcessorCache cache : caches) {
-                System.out.println("Cache Level: " + cache.getLevel());
+       
+        else if ((input1.contains("cache")) || (input1.contains("caches"))) {
+           
+            List<ProcessorCache> cache = processor.getProcessorCaches();
+          //  for (ProcessorCache cache : caches) {
+               /*  System.out.println("Cache Level: " + cache.getLevel());
                  System.out.println("Cache Type: " + cache.getType());
-                  System.out.println("Cache Size: " + cache.getCacheSize() + " bytes");      
+                  System.out.println("Cache Size: " + cache.getCacheSize() + " bytes");  */
+
+               //   int lvl = cache.getLevel();
+
+                  int option = 0;
+                
+
+                  while (option != 4) {
+                    
+                    System.out.print("Which cache information would you like to know about? 1, 2, 3: ");
+                      option = ise.nextInt();
+
+                      switch (option) {
+                          
+                        case 1:
+                        
+                             System.out.println("Cache Level: " + cache.getLevel());
+                              System.out.println("Cache Type: " + cache.getType());
+                               System.out.println("Cache Size: " + cache.getCacheSize() + " bytes");
+                               break;
+                         
+                         case 2:
+                            
+                             System.out.println("Cache Level: " + cache.getLevel());
+                              System.out.println("Cache Type: " + cache.getType());
+                               System.out.println("Cache Size: " + cache.getCacheSize() + " bytes");
+                                break;
+
+                         case 3:
+                            
+                            System.out.println("Cache Level: " + cache.getLevel());
+                              System.out.println("Cache Type: " + cache.getType());
+                               System.out.println("Cache Size: " + cache.getCacheSize() + " bytes");
+                                break;
+
+                         case 4:
+                              
+                            System.out.println("Exiting cache information menu.");
+                               break;
+
+                          default:
+                              System.out.println("Invalid option. Please choose 1, 2, or 3.");
+                               break;     
+                      }
+                  }
+
+               /*  if (cacheInput.equals("l" + lvl)) {
+                      System.out.println("Cache Level: " + cache.getLevel());
+                      System.out.println("Cache Type: " + cache.getType());
+                      System.out.println("Cache Size: " + cache.getCacheSize() + " bytes");  
+                  }
+*/
+
+
             }
         }
-        else if ((input.contains("all")) || (input.contains("cpu"))) {
+
+        else if ((input1.contains("all")) || (input1.contains("cpu"))) {
+
             System.out.println("=== CPU Information ===");
              System.out.println("Processor: " + processor.getProcessorIdentifier().getName());
               System.out.println("Physical Cores: " + processor.getPhysicalProcessorCount());
@@ -54,13 +115,12 @@ public class CPU {
                 System.out.println("Max Frequency: " + processor.getMaxFreq() + " GHz");
 
         } 
-        else if ((input.contains("processor")) || (input.contains("name"))) {
+
+        else if ((input1.contains("processor")) || (input1.contains("name"))) {
+
             System.out.println("Processor: " + processor.getProcessorIdentifier().getName());
-          //   System.out.println("ID Number: " + processor.getPhysicalProcessorNumber());
-         
-
-
-
+          //c   System.out.println("ID Number: " + processor.getPhysicalProcessorNumber());
+      
         }
         else {
             System.out.println("Invalid input. Please enter 'cores', 'frequency', or 'all'.");
